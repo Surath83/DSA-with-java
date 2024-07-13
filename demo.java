46eebffc02bfd1f11ca08a9f1e804bec800eb960
SURATH CHOWDHURY <133858119+Surath83@users.noreply.github.com>
2024-07-13 23:50:23 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class demo {
+    public static void main(String[] args) {
+        int n = Integer.MAX_VALUE;
+        int m = Integer.MIN_VALUE;
+        
+        System.out.println(n+1);
+        System.out.println(m-1);
+    }
+}
