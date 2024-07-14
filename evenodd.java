edf89b618d4b6f0bd884ac3b69e8e4308c5d68c6
SURATH CHOWDHURY <133858119+Surath83@users.noreply.github.com>
2024-07-14 11:37:38 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,20 @@
+/*import java.util.*;
+public class evenodd {
+        public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("how many no. will you enter ?");
+        int i = sc.nextInt();
+        System.out.println("Enter the number: ");
+        char even[100]
+        for(int j=0; j<i; j++){
+            int n = sc.nextInt();
+        }
+
+
+
+
+        sc.close();
+    }
+}*/
+
+    
