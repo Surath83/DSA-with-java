d70b240a4aa051446327b4f2228d0d40ac2d4cb3
SURATH CHOWDHURY <133858119+Surath83@users.noreply.github.com>
2024-07-30 14:56:18 +0530

    Add files via upload

---
+++ type.java
@@ -0,0 +1,12 @@
+public class type {
+    /**
+     * @param args
+     */
+    public static void main(String args[]) {
+        char a = 'a';
+        char b = 'b';
+        System.out.println((int) a);
+        System.out.println(b - a);
+    }
+
+}
